ee1b800e735eb1a52e736e97e5b0d9994a9ad4dc
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-08 00:12:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+
+
+
 function init() {
-  // your code here
+  var index=0;
+  document.body.addEventListener('keydown', function(e){
+    if (index < codes.length){
+      var key = e.key
+      if (key===codes[index]){
+       index++;
+       console.log(key)
+      }else{
+      index = 0;
+      console.log("Wrong code!")
+      }
+    }
+    if (index === codes.length){
+      alert("You got the Konami code!")
+    }
+  })
 }
